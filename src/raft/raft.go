31e1cbe560081f2f3eac80602ea5a4ec5f3e8997
root <suqinglee@outlook.com>
2021-10-03 00:13:57 +0800

    lab2A all passed

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -210,82 +210,6 @@ func (rf *Raft) election() {
 				rf.lastBroadcastLog = time.Unix(0, 0)
 				return
 			}
-
-			/*
-
-					// 请求投票req
-					args := RequestVoteArgs{
-						Term:         rf.currentTerm,
-						CandidateId:  rf.me,
-						LastLogIndex: len(rf.log),
-					}
-					if len(rf.log) != 0 {
-						args.LastLogTerm = rf.log[len(rf.log)-1].Term
-					}
-
-					rf.mu.Unlock()
-
-					// 并发RPC请求vote
-					type VoteResult struct {
-						peerId int
-						resp   *RequestVoteReply
-					}
-					voteCount := 1   // 收到投票个数（先给自己投1票）
-					finishCount := 1 // 收到应答个数
-					voteResultChan := make(chan *VoteResult, len(rf.peers))
-					for peerId := 0; peerId < len(rf.peers); peerId++ {
-						go func(id int) {
-							if id == rf.me {
-								return
-							}
-							resp := RequestVoteReply{}
-							if ok := rf.sendRequestVote(id, &args, &resp); ok {
-								voteResultChan <- &VoteResult{peerId: id, resp: &resp}
-							} else {
-								voteResultChan <- &VoteResult{peerId: id, resp: nil}
-							}
-						}(peerId)
-					}
-
-					maxTerm := 0
-					for {
-						select {
-						case voteResult := <-voteResultChan:
-							finishCount += 1
-							if voteResult.resp != nil {
-								if voteResult.resp.VoteGranted {
-									voteCount += 1
-								}
-								if voteResult.resp.Term > maxTerm {
-									maxTerm = voteResult.resp.Term
-								}
-							}
-							// 得到大多数vote后，立即离开
-							if finishCount == len(rf.peers) || voteCount > len(rf.peers)/2 {
-								goto VOTE_END
-							}
-						}
-					}
-				VOTE_END:
-					rf.mu.Lock()
-					// 如果角色改变了，则忽略本轮投票结果
-					if rf.role != Candidate {
-						return
-					}
-					// 发现了更高的任期，切回follower
-					if maxTerm > rf.currentTerm {
-						rf.role = Follower
-						rf.currentTerm = maxTerm
-						rf.votedFor = -1
-						return
-					}
-					// 赢得大多数选票，则成为leader
-					if voteCount > len(rf.peers)/2 {
-						rf.role = Leader
-						rf.lastBroadcastLog = time.Unix(0, 0) // 令appendEntries广播立即执行
-						return
-					}
-			*/
 		}()
 	}
 }
@@ -347,7 +271,6 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	// Your initialization code here (2A, 2B, 2C).
 	rf.role = Follower
 	rf.votedFor = -1
-	rf.lastReceiveLog = time.Now()
 
 	go rf.election()
 	go rf.sync()
